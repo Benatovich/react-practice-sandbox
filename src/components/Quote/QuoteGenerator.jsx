316016837f058585fa8cca5a26f7ddb3b8484dfc
Benatovich <sambenatovich@gmail.com>
2022-08-02 17:29:27 -0400

    quoteGenerator pulls a random futurama quote on click and returns the quote and name of the speaker

--- src/components/Quote/QuoteGenerator.jsx
+++ src/components/Quote/QuoteGenerator.jsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function QuoteGenerator() {
+    const [quote, setQuote] = useState('Fear is the mind-killer');
+    const [speaker, setSpeaker] = useState('Muad\'Dib');
+
+    async function handleClick() {
+        const url = 'https://futuramaquoteapi.herokuapp.com/randomquote';
+        const response = await fetch(url);
+        const json = await response.json();
+
+        console.log('json', json.quote)
+
+        setQuote(json.quote);
+        setSpeaker(json.name);
+    }
   return (
-    <div>"An inspirational quote"</div>
+    <>
+        <button onClick={handleClick}>fetch quote</button>
+        <div style={{ margin: 10 }}>"{quote}"</div>
+        <div style={{ margin: 10 }}>- {speaker}</div>
+    </>
   )
 }
